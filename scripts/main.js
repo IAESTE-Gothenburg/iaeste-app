effcbf9677850b16a4cb7cb51b41edd234d56aef
Anton Levholm <anton.badminton@gmail.com>
2017-02-04 14:23:50 +0100

    generate internship list with javascript

---
+++ scripts/main.js
@@ -0,0 +1,162 @@
+var data = {internships: [
+	{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+},
+{	
+		refNr: 'AT-2017-4004LE',
+		employer: 'Chair of Energy Network Technology, Montanuniversität Leoben',
+		country: 'Austria',
+		countryImage: 'images/flags/austria.png',
+		location: 'Leoben',
+		description: 'Large and complex energy systems are investigated for potential energy savings and the integration of renewable energy sources. Methodical process analysis is used to identify possible optimization fields and later on to evaluate effects and implications of any changes. Optimization fields might be the use of waste heat recovery, the integration of storages or the use of renewable energy sources. The trainee will be involved in a couple of projects in the topics stated above depend.',
+		studyLevel: 'End (7 semesters and over)',
+		languages: ['English (Excellent)', 'Other'],
+		website: '',
+		field: ['Electrical Engineering', 'Energy Engineering', 'Other'],
+		fieldAbbr: 'E',
+		specialization: ['Renewable Energies', 'Process- & Data Engineering', 'Energy System Engineering', 'Energy Storage'],
+		nrOfWeeks: '26 - 26',
+		period: '01-FEB-2017 - 30-NOV-2017'
+}
+]};
+
+var mainContent;
+
+document.addEventListener('DOMContentLoaded', function(){
+	mainContent = document.getElementsByTagName('main')[0];
+
+	addInternships();
+});
+
+function addInternships() {
+	for (var i = 0; i < data.internships.length; i++) {
+		// Creates container for internship
+		var container = document.createElement('div');
+		container.classList.add('internship');
+		// Creates left side content
+		var leftContent = document.createElement('div');
+		leftContent.classList.add('left-content');
+		var image = document.createElement('img');
+		image.src = data.internships[i].countryImage;
+		leftContent.appendChild(image);
+		var location = document.createElement('h3');
+		var textNode = document.createTextNode(data.internships[i].location)
+		location.appendChild(textNode);
+		leftContent.appendChild(location);
+		// Creates right side content
+		var rightContent = document.createElement('div');
+		rightContent.classList.add('right-content');
+		var employer = document.createElement('h2');
+		textNode = document.createTextNode(data.internships[i].employer)
+		employer.appendChild(textNode);
+		rightContent.appendChild(employer);
+		var text = document.createElement('h3');
+		textNode = document.createTextNode('Weeks Offered: ' + data.internships[i].nrOfWeeks)
+		text.appendChild(textNode);
+		rightContent.appendChild(text);
+		text = document.createElement('h3');
+		textNode = document.createTextNode('Within the months: ' + data.internships[i].period)
+		text.appendChild(textNode);
+		rightContent.appendChild(text);
+		// Append content
+		container.appendChild(leftContent);
+		container.appendChild(rightContent);
+		mainContent.appendChild(container);
+	}
+}
+
+
+
