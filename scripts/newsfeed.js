87acc587ec46f7fdf5cd07f9180ce2b09e5fbdc0
Anton Levholm <anton.badminton@gmail.com>
2017-02-24 19:14:02 +0100

    news view

--- scripts/newsfeed.js
+++ scripts/newsfeed.js
@@ -7,6 +7,7 @@ function addNewsfeed() {
 		var container = document.createElement('article');
 		container.classList.add('newsItem');
 		container.id = t.id;
+		container.onclick = viewNewsItem;
 
 		var image = document.createElement('img');
 		image.src = t.image;
@@ -27,6 +28,66 @@ function addNewsfeed() {
 		mainContent.appendChild(container);
 	}
 }
+
+function viewNewsItem(e) {
+	mainContent.scrollTop = 0;
+	var id = e.currentTarget.id;
+	mainContent.innerHTML = '';
+	for (var i = 0; i < news.story.length; i++) {
+		var t = news.story[i];
+		if (t.id == id) {
+			headerTitle.innerHTML = t.startDate;
+			headerIcon.style.display = 'none';
+
+			// Create container element
+			var container = document.createElement('article');
+			container.id = 'news-item-view';
+
+			// Information box
+			var box = document.createElement('section');
+
+			// Create news image
+			var image = document.createElement('img');
+			image.src = t.image;
+			box.appendChild(image);
+
+			// Event name
+			addTextElement('h2', t.name, box);
+
+			// Event time
+			var text = t.startTime + ' den ' + t.startDate + ' - ' + t.endTime + ' den ' + t.endDate;
+			addTextElement('p', text, box);
+
+			// Event location
+			addTextElement('p', t.location, box);
+
+			// Event link
+			var link = document.createElement('a');
+			link.href = '';
+			var icon = document.createElement('i');
+			icon.classList.add('fa');
+			icon.classList.add('fa-facebook');
+			link.appendChild(icon);
+			text = document.createTextNode(' Läs mer');
+			link.appendChild(text);
+			box.appendChild(link);
+
+			// Add box to container
+			container.appendChild(box);
+
+			// Event description
+			addTextElement('p', lorem, container);
+			addTextElement('p', lorem, container);
+			addTextElement('p', lorem, container);
+
+			navIcon.style.display = 'block';
+			mainContent.appendChild(container);
+			break;
+		}
+	}
+}
+
+var lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas placeat illo incidunt temporibus fuga excepturi nemo dicta, architecto nesciunt, tempora dignissimos nobis nisi et necessitatibus reiciendis omnis at facere praesentium!';
 
 var news = {
 	story: [
